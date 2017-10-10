e7879a7b63fb77db74a3acbfab11f6ce8209cdaf
Rich A. Marino <ram06@acuitysso.com>
2017-10-09 22:41:08 -0400

    made word have unsigned chars to deal with high ASCII characters.

--- c/c.c
+++ c/c.c
@@ -1,22 +1,46 @@
 #include <stdio.h>
 #include <malloc.h>
 
-#define MAX_WORD_LENGTH 255
+#define MAX_WORD_LENGTH 980
+#define CHARSET_SIZE 255
 
 //BUG: "..." at the end of a word is not stripped when counting the word.
 /*
 Discussion:
 A very fast method of creating a concordance, in close to O(n) time where 
-  n is the number of bytes in the document to be examined.
+ n is the number of characters in the document to be examined.  The actual
+ time complexity is approximately O(n * m), where n is the number of
+ characters in the document and m is the average length of each word.
+ (There is some overhead for dealing with unusual punctuation.)
+
+To increase the performance, checking for punctuation is done using arrays
+ that map to every ASCII character, allowing for a check in O(1) time.
 
 The document is read, and the words are split, accounting for acronyms
- (words with periods in the middle) being words.  Each word is added
- to the trie if it does not exist, and the count is increased.
+ (words with periods in the middle) being words.  If a word has not yet
+ been seen, it is added to the trie.  Otherwise it is found in the trie,
+ and the counter increased.  The current sentence number is also added 
+ to a Linked List that is associated with each unique word in the trie.
 
-Each trie node also contains a linked list containing a list of sentances
-where each word appears.
+This implementation expects an ASCII character set (up to 255 characters),
+ however, it could fairly easily be modified to work with wide characters.
+ More memory would be used, and startup would be slower, but the speed
+ should remain similar, aside from additional time for memory allocation
+ being required.
+
+Known issues:
+ An ellipsis does not count as an end of a sentence.  While an ellipsis
+  does not necessarily need to be the end of a sentence, no method of
+  attempting to determine whether an ellipsis is the end of a sentence
+  or not has been implemented.  The same is true for acronyms that contain
+  multiple periods. 
+ An abbreviation that ends in a period will not include the period when
+  it is added to the table.  This would only affect the count if the 
+  abbreviation happens to match a word, and the period is required to 
+  tell the difference.
+
 */
-const char * filename = "../input/test2.txt";
+const char * filename = "../input/war_and_peace.txt";
 
 //A LinkedList Node used for storing the list of sentences that 
 //contain the word.  Doublely-Linked allows us to traverse the list
@@ -33,7 +57,7 @@ struct trie_node{
   int counter;
   struct ll_node * first;
   struct ll_node * last;
-  struct trie_node* children[255];
+  struct trie_node* children[ CHARSET_SIZE ];
 };
 
 //This will hold the root trie node.  It is not to be edited once
@@ -41,22 +65,30 @@ struct trie_node{
 struct trie_node * root;
 
 /*
-  Utility function to help printing the trie. 
+  Recursive utility function to help printing the trie. 
 
+  node is the trie_node to print from.
+  word is a buffer that is used for printing the current word.
   Depth is used to deal with adjusting word.
 */
 void print_trie( struct trie_node * node, char * word, int depth )
 {
   int i;
+  struct ll_node* lln;
   if( node->counter > 0 )
   {
-    printf("%.*s%s\t\t{%d", depth, word,
+    printf("%.*s%s\t\t{%d:", depth, word,
       (depth > 7)?"":"\t", node->counter );
     //read the linked list
+    lln = node->first;
+    do{
+      printf("%s%d", (lln==node->first)?"":",", lln->sentance_number);
+      lln = lln->next;
+    }while( lln != NULL );
     printf("}\n");
   }
   
-  for( i = 0; i < 255; ++i )
+  for( i = 0; i < CHARSET_SIZE; ++i )
   {
     if( node->children[i] != NULL )
     {
@@ -72,14 +104,14 @@ int main()
 
   //the char we are reading right now.
   char c;
-  char * word = malloc( MAX_WORD_LENGTH );
+  unsigned char * word = malloc( MAX_WORD_LENGTH );
   int word_length = 0;
 
   int sentance_counter = 1;
   int queue_increase_sentance_counter = 0;
 
   char sentance_ending_symbols[] = {'.', '?', '!'};
-  char sep[] = {' ', ',', '\n', '\r', '(', ')', '[', ']', '{', '}', ':', ';'};
+  char sep[] = {' ', ',', '\n', '\r', '(', ')', '[', ']', '{', '}', ':', ';', EOF};
   char other_trim_symbols[] = {'-', '=', '\'', '\"'};
 
   /*
@@ -87,16 +119,16 @@ int main()
     a map, so we can do an O(1) lookup.  This would take up more memory
     in a unicode space, but would still work.
   */
-  char sep_map[255];
-  char sentance_ending_map[255];
-  char all_trim_symbols_map[255];
+  char sep_map[CHARSET_SIZE];
+  char sentance_ending_map[CHARSET_SIZE];
+  char all_trim_symbols_map[CHARSET_SIZE];
   
   //temporary variables
   int i = 0;
   struct trie_node * ct;
 
   //clear the arrays.
-  for(i = 0; i < 255; ++i) 
+  for(i = 0; i < CHARSET_SIZE; ++i) 
   {
     sep_map[i] = 0;
     sentance_ending_map[i] = 0;
@@ -129,7 +161,9 @@ int main()
     fprintf(stderr, "Can't open file.");
   }
 
-  do{
+  while( c != EOF )
+  {
+  //do{
     c = fgetc( ifp );
     //Check for end of sentance.
     if( sentance_ending_map[ c ] )
@@ -215,13 +249,9 @@ int main()
         fprintf( stderr, "There is a word longer than the maximum word length.");
       }
     }
-    //printf("%c", c);
-  }while (c != EOF );
+  }//end of main while loop
 
-  //Now print out the contents of the trie.
-  //We will reuse the word buffer.
-  word = "---------------";
+  //Now print out the contents of the trie, reusing the word buffer.
   print_trie( root, word, 0 );
   printf("Done.\n");
-  free( word );
 }
