d19c93354ff1f4b2a4825ed5911bdb03ef5b9ca5
Rich A. Marino <ram06@acuitysso.com>
2017-10-09 21:38:02 -0400

    The basics of the c function now work.

--- c/c.c
+++ c/c.c
@@ -2,9 +2,70 @@
 #include <malloc.h>
 
 #define MAX_WORD_LENGTH 255
-const char * filename = "../input/test2.txt";
 
 //BUG: "..." at the end of a word is not stripped when counting the word.
+/*
+Discussion:
+A very fast method of creating a concordance, in close to O(n) time where 
+  n is the number of bytes in the document to be examined.
+
+The document is read, and the words are split, accounting for acronyms
+ (words with periods in the middle) being words.  Each word is added
+ to the trie if it does not exist, and the count is increased.
+
+Each trie node also contains a linked list containing a list of sentances
+where each word appears.
+*/
+const char * filename = "../input/test2.txt";
+
+//A LinkedList Node used for storing the list of sentences that 
+//contain the word.  Doublely-Linked allows us to traverse the list
+//in reverse order -- so that the list appears in increasing order
+//without needing to be sorted.
+struct ll_node
+{
+  int sentance_number;
+  struct ll_node * next;
+//  struct ll_node * prev;
+};
+
+//A trie will be used to hold the word list.
+struct trie_node{
+  int counter;
+  struct ll_node * first;
+  struct ll_node * last;
+  struct trie_node* children[255];
+};
+
+//This will hold the root trie node.  It is not to be edited once
+//initialized.
+struct trie_node * root;
+
+/*
+  Utility function to help printing the trie. 
+
+  Depth is used to deal with adjusting word.
+*/
+void print_trie( struct trie_node * node, char * word, int depth )
+{
+  int i;
+  if( node->counter > 0 )
+  {
+    printf("%.*s%s\t\t{%d", depth, word,
+      (depth > 7)?"":"\t", node->counter );
+    //read the linked list
+    printf("}\n");
+  }
+  
+  for( i = 0; i < 255; ++i )
+  {
+    if( node->children[i] != NULL )
+    {
+      word[depth] = (char)i;
+      print_trie( node->children[i], word, depth+1);
+    }
+  }
+}
 int main()
 {
   //file pointer
@@ -31,7 +92,9 @@ int main()
   char sentance_ending_map[255];
   char all_trim_symbols_map[255];
   
+  //temporary variables
   int i = 0;
+  struct trie_node * ct;
 
   //clear the arrays.
   for(i = 0; i < 255; ++i) 
@@ -56,6 +119,9 @@ int main()
     all_trim_symbols_map[ other_trim_symbols[i] ] = 1;
   }
 
+  //prepare the trie.
+  root = (struct trie_node*) calloc( 1, sizeof( struct trie_node ) );
+
   //Open file
   ifp = fopen( filename, "r" );
 
@@ -69,7 +135,6 @@ int main()
     //Check for end of sentance.
     if( sentance_ending_map[ c ] )
     {
-      printf("$");
       ++queue_increase_sentance_counter;
     }
 
@@ -87,14 +152,27 @@ int main()
       }
   
       //Add the word.
-      printf("\"%.*s\"\n", word_length, word);
-      word_length = 0;
-
+      if( word_length > 0 )
+      {
+        i = 0;
+        ct = root;
+        do{
+          if( ct->children[ word[i] ] == NULL )
+          {
+            ct->children[ word[i] ] = 
+              (struct trie_node*) calloc( 1, sizeof( struct trie_node ) );
+          }
+          ct = ct->children[ word[i] ];
+          ++i;
+        }while( i < word_length );
+        ++( ct->counter );
+        //printf("\"%.*s\"\n", word_length, word);
+      }//if( word_length > 0 )
       if( queue_increase_sentance_counter == 1 )
       {
-        printf("NEW sentence\n");
+        ++sentance_counter;
       }
-
+      word_length = 0;
       queue_increase_sentance_counter = 0;
     }
     else
@@ -128,6 +206,10 @@ int main()
     //printf("%c", c);
   }while (c != EOF );
 
+  //Now print out the contents of the trie.
+  //We will reuse the word buffer.
+  word = "---------------";
+  print_trie( root, word, 0 );
   printf("Done.\n");
   free( word );
 }
